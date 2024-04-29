9e6c732cb95be6373bf3a8f98772adb9b25f610c
Eilliar <engbrunoeilliar@gmail.com>
2024-04-29 14:54:41 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,43 @@
+
+
+class Hero():
+
+    def __init__(self) -> None:
+        self.name = ""
+        self.exp = 0
+        self.lvl = self.get_tag()
+
+    def start(self):
+        self.exp = int(input("What is the here experience points? "))
+        self.lvl = self.get_tag()
+
+    def get_tag(self):
+        if self.exp <= 1000:
+            return "Ferro"
+        elif 1001 <= self.exp <= 2000:
+            return "Bronze"
+        elif 2001 <= self.exp <= 5000:
+            return "Prata"
+        elif 5001 <= self.exp <= 7000:
+            return "Ouro"
+        elif 7001 <= self.exp <= 8000:
+            return "Platina"
+        elif 8001 <= self.exp <= 9000:
+            return "Ascendente"
+        elif 9001 <= self.exp <= 10000:
+            return "Imortal"
+        elif self.exp >= 10001:
+            return "Radiante"
+        else:
+            return "Error"
+
+    def execute(self):
+        self.start()
+        print(self)
+
+    def __str__(self) -> str:
+        return f"O Herói de nome {self.name} está no nível {self.lvl}"
+
+
+if __name__ == "__main__":
+    Hero().execute()
